10bec2e7ae85485acb324c885cd73146e6aeabbb
danteidbratt <danteidbratt@users.noreply.github.com>
2017-11-29 22:48:34 +0100

    chaos

--- src/tetris/Game.java
+++ src/tetris/Game.java
@@ -221,17 +221,9 @@ public class Game extends JPanel implements Runnable {
     public void fall(int y){
         for (; y > 1; y--) {
             for (int j = 1; j < grid[y].length-1; j++) {
-                System.out.println("hej");
-                if (grid[y-1][j].block == null){
-                    grid[y][j].block = null;
-                    grid[y][j].isOccupied = false;
-                    grid[y][j].setBackground(backgroundColor);
-                }
-                else {
-                    grid[y][j].block = grid[y-1][j].block;
-                    grid[y][j].isOccupied = true;
-                    grid[y][j].adaptToBlock();
-                }
+                grid[y][j].block = grid[y-1][j].block;
+                grid[y][j].isOccupied = grid[y-1][j].isOccupied;
+                grid[y][j].adaptToBlock();
                 grid[y-1][j].block = null;
                 grid[y-1][j].isOccupied = false;
                 grid[y-1][j].setBackground(backgroundColor);
