b152f8981a6f552a24aa908e9bed70c6d512f23c
danteidbratt <danteidbratt@users.noreply.github.com>
2017-11-29 20:17:21 +0100

    rotate dante

--- src/tetris/Figure.java
+++ src/tetris/Figure.java
@@ -4,11 +4,13 @@ public class Figure {
     
     Block[] blocks = new Block[4];
     Shape shape;
+    boolean standing;
     
 
     public Figure(Block[] blocks, Shape shape) {
         this.blocks = blocks;
         this.shape = shape;
+//        if (shape == )
     }
     
     public void rotate(){
