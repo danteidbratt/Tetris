0de9d6492acc69cc08f037acd6d719c10a8731d1
Jakob <jakob.lindahl@yh.nackademin.se>
2017-11-29 19:15:54 +0100

    s

--- src/tetris/FigureFactory.java
+++ src/tetris/FigureFactory.java
@@ -4,73 +4,10 @@ public class FigureFactory {
 
     Block[] blocks;
     Figure figure;
-//    int[][][] presets = new int[7][4][2];
 
     public FigureFactory() {
         blocks = new Block[4];
 
-//        presets[0][0][0] = 1;
-//        presets[0][0][1] = 5;
-//        presets[0][1][0] = 1;
-//        presets[0][1][1] = 6;
-//        presets[0][2][0] = 2;
-//        presets[0][2][1] = 5;
-//        presets[0][3][0] = 2;
-//        presets[0][3][1] = 6;
-//                
-//        presets[1][0][0] = 1;
-//        presets[1][0][1] = 5;
-//        presets[1][1][0] = 2;
-//        presets[1][1][1] = 5;
-//        presets[1][2][0] = 3;
-//        presets[1][2][1] = 5;
-//        presets[1][3][0] = 4;
-//        presets[1][3][1] = 5;
-//                
-//        presets[2][0][0] = 1;
-//        presets[2][0][1] = 5;
-//        presets[2][1][0] = 1;
-//        presets[2][1][1] = 6;
-//        presets[2][2][0] = 2;
-//        presets[2][2][1] = 5;
-//        presets[2][3][0] = 2;
-//        presets[2][3][1] = 6;
-//                
-//        presets[3][0][0] = 1;
-//        presets[3][0][1] = 5;
-//        presets[3][1][0] = 1;
-//        presets[3][1][1] = 6;
-//        presets[3][2][0] = 2;
-//        presets[3][2][1] = 5;
-//        presets[3][3][0] = 2;
-//        presets[3][3][1] = 6;
-//                
-//        presets[4][0][0] = 1;
-//        presets[4][0][1] = 5;
-//        presets[4][1][0] = 1;
-//        presets[4][1][1] = 6;
-//        presets[4][2][0] = 2;
-//        presets[4][2][1] = 5;
-//        presets[4][3][0] = 2;
-//        presets[4][3][1] = 6;
-//                
-//        presets[5][0][0] = 1;
-//        presets[5][0][1] = 5;
-//        presets[5][1][0] = 1;
-//        presets[5][1][1] = 6;
-//        presets[5][2][0] = 2;
-//        presets[5][2][1] = 5;
-//        presets[5][3][0] = 2;
-//        presets[5][3][1] = 6;
-//                
-//        presets[6][0][0] = 1;
-//        presets[6][0][1] = 5;
-//        presets[6][1][0] = 1;
-//        presets[6][1][1] = 6;
-//        presets[6][2][0] = 2;
-//        presets[6][2][1] = 5;
-//        presets[6][3][0] = 2;
-//        presets[6][3][1] = 6;
     }
 
     public void createFigure(int x) {
