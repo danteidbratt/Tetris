0ad1c3c1b787ddc487bcaddff6a5df1d016e09de
danteidbratt <danteidbratt@users.noreply.github.com>
2017-11-29 20:36:25 +0100

    De enkla går att rotera

--- src/tetris/Figure.java
+++ src/tetris/Figure.java
@@ -12,10 +12,7 @@ public class Figure {
     public Figure(Block[] blocks, Shape shape) {
         this.blocks = blocks;
         this.shape = shape;
-        if (shape == STICK)
-            standing = true;
-        else
-            standing = false;
+        standing = false;
     }
     
     public void rotate(){
@@ -25,20 +22,20 @@ public class Figure {
                 break;
             case STICK:
                 if (standing) {
-                    blocks[0].x -= 1;
                     blocks[0].y += 1;
-                    blocks[2].x += 1;
+                    blocks[0].x -= 1;
                     blocks[2].y -= 1;
+                    blocks[2].x += 1;
+                    blocks[3].y -= 2;
                     blocks[3].x += 2;
-                    blocks[2].y -= 2;
                 }
                 else {
-                    blocks[0].x += 1;
                     blocks[0].y -= 1;
-                    blocks[2].x -= 1;
+                    blocks[0].x += 1;
                     blocks[2].y += 1;
+                    blocks[2].x -= 1;
+                    blocks[3].y += 2;
                     blocks[3].x -= 2;
-                    blocks[2].y += 2;
                 }
                 break;
             case L:
@@ -49,28 +46,28 @@ public class Figure {
                 break;
             case Z:
                 if (standing) {
-                    blocks[0].x -= 2;
                     blocks[3].y += 2;
+                    blocks[0].x -= 2;
                 }
                 else {
-                    blocks[0].x += 2;
                     blocks[3].y -= 2;
+                    blocks[0].x += 2;
                 }
                 break;
             case REVERSE_Z:
                 if (standing) {
-                    blocks[1].x += 2;
                     blocks[2].y += 2;
+                    blocks[1].x += 2;
                 }
                 else {
-                    blocks[1].x -= 2;
                     blocks[2].y -= 2;
+                    blocks[1].x -= 2;
                 }
                 break;
             case T:
                 
                 break;
         }
-        standing = !standing;
+        this.standing = !standing;
     }
 }
